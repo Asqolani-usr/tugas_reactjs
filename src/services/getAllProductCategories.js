200a952473c33f919f05ffee838fbbb7c93b2c43
olan <asqolani2@gmail.com>
2024-11-18 20:09:23 +0800

    siuu

---
+++ src/services/getAllProductCategories.js
@@ -0,0 +1,13 @@
+export default function getAllProductCategories(){
+  return  [   {
+      id: 'jacket',
+      slug: 'outdor_jacket',
+      name: `Outdor's jacket`
+    },
+    {
+      id: 'tent',
+      slug: 'camping-tent',
+      name: `Camping's tent`
+    },
+  ]
+}
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,63 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'jacket-001',
+      slug: 'The North Face Women Circaloft Jacket',
+      name: "The North Face Women Circaloft Jacket-NF0A84JBJK3",
+      category: `Outdor's jacket`,
+      categorySlug: 'outdor_jacket',
+      price: 3_703_000,
+      stock: 100,
+      imageUrl: '/assets/images/944aa5cd-1bf6-42ba-8f11-ebfc29f655d4.jpg',
+      description: `The North Face Women Circaloft Jacket-NF0A84JBJK3 Circaloft, produk terbaru kami, adalah lini isolasi sintetis yang ringan, mudah dipadatkan, dan sangat mudah dibawa.
+`
+    },
+    {
+      id: 'jacket-002',
+      slug: '"The North Face Men Sierra Peak Pro Jacket',
+      name: "The North Face Men Sierra Peak Pro Jacket-NF0A7W7N173 ",
+      category: `Outdor's jacket`,
+      categorySlug: 'outdor_jacket',
+      price: 4_963_000,
+      stock: 50,
+      imageUrl: `/assets/images/2d56bf89-55e1-4acd-bc6e-8940b232d7e8.jpg`,
+      description: `The North Face Men Sierra Peak Pro Jacket-NF0A7W7N173, Lawan cuaca dingin dengan kehangatan luar biasa dari jaket ini.`
+    },
+    {
+      id: 'tent-001',
+      slug: '"The North Face Unisex Homestead Shelter-tent',
+      name: "The North Face Unisex Homestead Shelter-NF0A81D5KOK",
+      category: `Camping's tent`,
+      categorySlug: 'camping-tent',
+      price: 10_890_000,
+      stock: 3,
+      imageUrl: `/assets/images/749af1a3-a16c-4825-9106-d5726cef3344.jpg`,
+      description: `The North Face Unisex Homestead Shelter-NF0A81D5KOK \n\n Shelter ini memiliki tinggi plafon 213 cm yang memungkinkan kamu bekerja sambil berdiri dengan nyaman. Selain struktur tiang silang yang mudah dipasang, terdapat dua tiang pendek di bagian atap untuk menjaga kenyamanan ruang dalam. Ukurannya cukup besar untuk menampung meja dari area perkemahan, dan kamu dapat mendirikan dapur atau ruang makan di dalamnya. Tali penguat di keempat sudut hadir sebagai perlengkapan standar untuk meningkatkan ketahanan terhadap angin. Bagian dalamnya dilengkapi pintu masuk besar berbahan mesh dan gantungan untuk menyimpan peralatan serta lentera elektronik. Tas penyimpanan tipe duffel yang praktis membuat shelter ini mudah disimpan dan dibawa.
+`
+    },
+    {
+      id: 'tent-002',
+      slug: 'The North Face Unisex Wawona-tent',
+      name: "The North Face Unisex Wawona-NF0A52WBY10",
+      category: `Camping's tent`,
+      categorySlug: 'camping-tent',
+      price: 15_900_000,
+      stock: 0,
+      imageUrl: `/assets/images/817cf2c5-8a1a-4476-a3a4-908758ce51d7.jpg`,
+      description: `Tenda berkualitas tinggi untuk pengalaman berkemah yang nyaman.\n\nDibuat dengan bahan tahan lama dan tahan air, tenda ini cocok untuk berbagai kondisi cuaca.\n\nFitur unggulan:\n- Mudah dipasang\n- Kapasitas 4 orang\n- Ventilasi yang baik\n- Tahan air`
+    },
+    {
+      id: 'jacket-003',
+      slug: 'The North Face Women Travel Triclimate Jacket-tent',
+      name: "The North Face Women Travel Triclimate Jacket-NF0A7QSMKX7",
+      category: `Outdor's jacket`,
+      categorySlug: 'outdor_jacket',
+      price: 4_712_000,
+      stock: 10,
+      imageUrl: `/assets/images/2762a505-5dec-4a23-aebc-aac96b416fda.jpg`,
+      description: `The North Face Women Travel Triclimate Jacket-NF0A7QSMKX7 \n\n Triclimate adalah jaket serbaguna untuk segala cuaca yang sempurna untuk hampir semua kondisi.
+`
+    },
+
+  ]
+}
